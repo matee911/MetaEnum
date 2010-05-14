9498236818226e0b1a1862bfe74aba1466c8fe88
matee <matee@matee.net>
2010-05-14 16:26:45 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+setup(name='MetaEnum',
+      description='',
+      keywords='',
+      version='0.1',
+      license='MIT',
+      url='http://github.com/matee911/MetaEnum', # home page for the package
+      download_url='http://github.com/matee911/MetaEnum/downloads',
+      maintainer='Mateusz `matee` Pawlik',
+      maintainer_email='matee@matee.net',
+      long_description='''
+      ''',
+      requires=[],
+      #package_dir={'':'src'},
+      py_modules=['metaenum'],
+      platforms=['Any'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+      ],)
